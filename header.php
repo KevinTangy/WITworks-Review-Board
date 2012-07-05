63cc6363342636da6e598d296907899ac95a19fe
Kevin Tang <KevynT@gmail.com>
2012-07-05 14:39:34 -0400

    Switched to minified versions of js and css files.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- Le styles -->
-		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<style type="text/css">
 			html, body
 			{
@@ -45,7 +45,7 @@
 				footer .container { padding-left: 20px; padding-right: 20px; }
 			}
 		</style>
-		<link href="css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 		<!-- <link href="css/custom.css" rel="stylesheet"> -->
 	
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
